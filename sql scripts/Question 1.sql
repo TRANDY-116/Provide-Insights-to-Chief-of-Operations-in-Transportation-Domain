3d2852d7c964eaff83b84993be536cf4db0cfd9e
TRANDY-116 <randyfun16@gmail.com>
2025-01-08 16:06:04 +0100

    top 3 and least 3 cities in terms of number of trips

---
+++ sql scripts/Question 1.sql
@@ -0,0 +1,19 @@
+-- Top 3 cities and 3 least cities in terms of trip counts
+WITH trip_counts AS (
+    SELECT 
+        COUNT(ft.trip_id) AS trip_count,
+        ft.city_id,
+        dc.city_name,
+        ROW_NUMBER() OVER (ORDER BY COUNT(ft.trip_id) DESC) AS rank_desc,
+        ROW_NUMBER() OVER (ORDER BY COUNT(ft.trip_id) ASC) AS rank_asc
+    FROM trips_db.fact_trips ft
+    JOIN trips_db.dim_city dc ON ft.city_id = dc.city_id
+    GROUP BY ft.city_id, dc.city_name
+)
+SELECT 
+    city_name,
+	trip_count
+FROM trip_counts
+WHERE rank_desc <= 3  -- Top 3 cities
+   OR rank_asc <= 3    -- Least 3 cities
+ORDER BY trip_count DESC;
