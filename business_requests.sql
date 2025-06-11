2727728c7c833bd2ac5031e255d2f02404a4b3a3
TRANDY-116 <randyfun16@gmail.com>
2025-06-11 22:26:12 +0100

    Now we need to do a report on power point and a video. Dashboard on excel

--- business_requests.sql
+++ business_requests.sql
@@ -211,4 +211,150 @@ GROUP BY
 ORDER BY
 	city_name;
 -- Business Request - 4: Identify Cities with Highest and Lowest Total New
+WITH CityTotals AS (
+    SELECT 
+        dc.city_name,
+        SUM(fps.new_passengers) AS total_new_passengers
+    FROM 
+        trips_db.fact_passenger_summary fps
+    JOIN 
+        trips_db.dim_city dc
+    ON 
+        fps.city_id = dc.city_id
+    GROUP BY 
+        dc.city_name
+),
+RankedCities AS (
+    SELECT 
+        city_name,
+        total_new_passengers,
+        RANK() OVER (ORDER BY total_new_passengers DESC) AS rank_high,
+        RANK() OVER (ORDER BY total_new_passengers ASC) AS rank_low
+    FROM 
+        CityTotals
+)
+SELECT 
+    city_name,
+    total_new_passengers,
+    CASE 
+        WHEN rank_high <= 3 THEN 'Top 3'
+        WHEN rank_low <= 3 THEN 'Bottom 3'
+        ELSE NULL
+    END AS city_category
+FROM 
+    RankedCities
+WHERE 
+    rank_high <= 3 OR rank_low <= 3
+ORDER BY 
+    city_category DESC, 
+    total_new_passengers DESC;
 
+-- Business Request - 5: Identify Month with Highest Revenue for Each City
+WITH CityMonthlyRevenue AS (
+    SELECT 
+        dc.city_name,
+        dd.month_name,
+        SUM(ft.fare_amount) AS monthly_revenue
+    FROM 
+        trips_db.fact_trips ft
+    JOIN 
+        trips_db.dim_city dc
+    ON 
+        ft.city_id = dc.city_id
+    JOIN 
+        trips_db.dim_date dd
+    ON 
+        ft._date = dd._date
+    GROUP BY 
+        dc.city_name, dd.month_name
+),
+CityTotalRevenue AS (
+    SELECT 
+        city_name,
+        SUM(monthly_revenue) AS total_revenue
+    FROM 
+        CityMonthlyRevenue
+    GROUP BY 
+        city_name
+),
+CityHighestRevenue AS (
+    SELECT 
+        cmr.city_name,
+        cmr.month_name AS highest_revenue_month,
+        cmr.monthly_revenue AS revenue,
+        ctr.total_revenue,
+        ROUND((cmr.monthly_revenue * 100.0 / ctr.total_revenue), 2) AS percentage_contribution
+    FROM 
+        CityMonthlyRevenue cmr
+    JOIN 
+        CityTotalRevenue ctr
+    ON 
+        cmr.city_name = ctr.city_name
+    WHERE 
+        cmr.monthly_revenue = (
+            SELECT MAX(monthly_revenue)
+            FROM CityMonthlyRevenue
+            WHERE city_name = cmr.city_name
+        )
+)
+SELECT 
+    city_name,
+    highest_revenue_month,
+    revenue,
+    percentage_contribution
+FROM 
+    CityHighestRevenue
+ORDER BY 
+    city_name;
+
+-- Business Request - 6: Repeat Passenger Rate Analysis
+
+WITH MonthlyRepeatRate AS (
+    SELECT
+        dc.city_name,
+        dd.month_name,
+        EXTRACT(MONTH FROM fps._month)::int AS month_num,
+        fps.total_passengers,
+        fps.repeat_passengers,
+        ROUND((fps.repeat_passengers * 100.0 / fps.total_passengers), 2) AS monthly_repeat_passenger_rate
+    FROM
+        trips_db.fact_passenger_summary fps
+    JOIN
+        trips_db.dim_city dc
+    ON
+        fps.city_id = dc.city_id
+	JOIN
+		trips_db.dim_date dd
+	ON
+		fps._month = dd._date
+),
+CityWideRepeatRate AS (
+    SELECT
+        dc.city_name,
+        SUM(fps.total_passengers) AS total_city_passengers,
+        SUM(fps.repeat_passengers) AS total_city_repeat_passengers,
+        ROUND((SUM(fps.repeat_passengers) * 100.0 / SUM(fps.total_passengers)), 2) AS city_repeat_passenger_rate
+    FROM
+        trips_db.fact_passenger_summary fps
+    JOIN
+        trips_db.dim_city dc
+    ON
+        fps.city_id = dc.city_id
+    GROUP BY
+        dc.city_name
+)
+SELECT
+    mrr.city_name,
+    mrr.month_name as month,
+    mrr.total_passengers,
+    mrr.repeat_passengers,
+    mrr.monthly_repeat_passenger_rate,
+    cwr.city_repeat_passenger_rate
+FROM
+    MonthlyRepeatRate mrr
+JOIN
+    CityWideRepeatRate cwr
+ON
+    mrr.city_name = cwr.city_name
+ORDER BY
+    mrr.city_name, mrr.month_num;
